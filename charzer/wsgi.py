98d9133e65470a2400d5df17f638f29c37b7a297
aftab <aftabahmad41442@gmail.com>
2022-08-29 00:01:05 +0530

    dj_static

--- charzer/wsgi.py
+++ charzer/wsgi.py
@@ -7,10 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
+from dj_static import Cling
 import os
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'charzer.settings')
 
-application = get_wsgi_application()
+
+application = Cling(get_wsgi_application())
